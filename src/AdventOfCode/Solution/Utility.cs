57e0e4f12339081fb90c9e699b99433f9c1f82c1
Timothy Williams <raziaar@gmail.com>
2023-11-20 09:48:14 -0600

    Add utility class for user's cross-day concerns

---
+++ src/AdventOfCode/Solution/Utility.cs
@@ -0,0 +1,6 @@
+﻿namespace AdventOfCode.Solution;
+
+internal static class Utility
+{
+	// Add your own cross-cutting utility functions here, or create your own classes. Namespace AdventOfCode.Solution automatically added to each Day's using statements.
+}
