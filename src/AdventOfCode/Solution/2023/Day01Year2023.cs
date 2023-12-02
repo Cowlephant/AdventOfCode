c631a1cebe7e8b44c3a173519a6985123b3914a7
Timothy Williams <raziaar@gmail.com>
2023-12-02 00:57:28 -0600

    Add solutions for days 1 and 2 for 2023

--- src/AdventOfCode/Solution/2023/Day01Year2023.cs
+++ src/AdventOfCode/Solution/2023/Day01Year2023.cs
@@ -5,19 +5,107 @@ namespace AdventOfCode.Solution;
 [AoCYearDay(2023, 1)]
 public sealed class Day01Year2023 : IAoCDaySolver
 {
-	[AoCExpectedExampleAnswers("")]
+	[AoCExpectedExampleAnswers("142")]
 	public string SolvePartOne(List<string> input)
 	{
-            // Solve
-
-		return "Not Implemented";
+		return SumFirstLastDigits(input);
 	}
 
-	[AoCExpectedExampleAnswers("")]
+	[AoCExpectedExampleAnswers("281")]
 	public string SolvePartTwo(List<string> input)
 	{
-            // Solve
+		Dictionary<string, int> wordsDigits = new Dictionary<string, int>
+		{
+			{ "one", 1 },
+			{ "two", 2 },
+			{ "three", 3 },
+			{ "four", 4 },
+			{ "five", 5 },
+			{ "six", 6 },
+			{ "seven", 7 },
+			{ "eight", 8 },
+			{ "nine", 9 },
+			{ "1", 1 },
+			{ "2", 2 },
+			{ "3", 3 },
+			{ "4", 4 },
+			{ "5", 5 },
+			{ "6", 6 },
+			{ "7", 7 },
+			{ "8", 8 },
+			{ "9", 9 }
+		};
 
-		return "Not Implemented";
+		List<int> modifiedInput = new();
+
+		for (int i = 0; i < input.Count; i++)
+		{
+			Dictionary<int, int> words = new();
+
+			foreach (var word in wordsDigits)
+			{
+				var index = input[i].IndexOf(word.Key);
+
+				if (index > -1)
+				{
+					words.Add(index, word.Value);
+				}
+			}
+
+			foreach (var word in wordsDigits)
+			{
+				var index = input[i].LastIndexOf(word.Key);
+
+				if (index > -1)
+				{
+					words.TryAdd(index, word.Value);
+				}
+			}
+
+			var digitsOrdered = words.OrderBy(w => w.Key);
+			var earliestDigit = digitsOrdered.First();
+			var latestDigit = digitsOrdered.Last();
+			var parsedValue = $"{earliestDigit.Value}{latestDigit.Value}";
+			modifiedInput.Add(int.Parse(parsedValue));
+		}
+
+		return modifiedInput.Sum().ToString();
+	}
+
+	private string SumFirstLastDigits(List<string> input)
+	{
+		List<int> digits = new List<int>();
+
+		for (var i = 0; i < input.Count; i++)
+		{
+			int firstDigit = 0;
+			int lastDigit = 0;
+
+			// Get first digit
+			for (var j = 0; j < input[i].Length; j++)
+			{
+				var item = input[i][j].ToString();
+				if (int.TryParse(item, out int number))
+				{
+					firstDigit = number;
+					break;
+				}
+			}
+
+			// Get last digit
+			for (var j = (input[i].Length) - (1); j >= 0; j--)
+			{
+				var item = input[i][j].ToString();
+				if (int.TryParse(item, out int number))
+				{
+					lastDigit = number;
+					break;
+				}
+			}
+
+			digits.Add(int.Parse($"{firstDigit}{lastDigit}"));
+		}
+
+		return digits.Sum().ToString();
 	}
 }
---
+++ src/AdventOfCode/Solution/2023/Day02Year2023.cs
@@ -0,0 +1,132 @@
+using AdventOfCode.Core;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Solution;
+
+[AoCYearDay(2023, 2)]
+public sealed class Day02Year2023 : IAoCDaySolver
+{
+	[AoCExpectedExampleAnswers("8")]
+	public string SolvePartOne(List<string> input)
+	{
+		int possibleGameIdCounts = 0;
+
+		foreach (var line in input)
+		{
+			Dictionary<string, int> blockColorCounts = new()
+			{
+				{ "red", 0 },
+				{ "green", 0 },
+				{ "blue", 0 }
+			};
+
+			Dictionary<string, int> blockCountThresholds = new()
+			{
+				{ "red", 12 },
+				{ "green", 13 },
+				{ "blue", 14 }
+			};
+
+			var indexMatch = new Regex(@"\d+").Match(line).Value;
+			var gameIndex = int.Parse(indexMatch);
+			var modifiedLine = Regex.Replace(line, @"^Game \d+: ", string.Empty);
+
+			int currentBlockCount = 0;
+			bool isGamePossible = true;
+
+			var lineChunks = modifiedLine
+				.Replace(";", " ;")
+				.Split(' ')
+				.Select(s => s.Replace(",", string.Empty));
+
+			foreach (var chunk in lineChunks)
+			{
+				// Chunk is a delimiter, starting new set
+                if (chunk == ";")
+                {
+					blockColorCounts["red"] = 0;
+					blockColorCounts["green"] = 0;
+					blockColorCounts["blue"] = 0;
+					continue;
+				}
+                // Chunk is a block count
+                if (int.TryParse(chunk, out int blockCount))
+				{
+					currentBlockCount = blockCount;
+				}
+				// Chunk is a block color descriptor
+				else
+				{
+					blockColorCounts[chunk] += currentBlockCount;
+					if (blockColorCounts[chunk] > blockCountThresholds[chunk])
+					{
+						isGamePossible = false;
+						break;
+					}
+				}
+			}
+
+			if (isGamePossible)
+			{
+				possibleGameIdCounts += gameIndex;
+			}
+		}
+
+		return possibleGameIdCounts.ToString();
+	}
+
+	[AoCExpectedExampleAnswers("2286")]
+	public string SolvePartTwo(List<string> input)
+	{
+		var possibleGamePowers = 0;
+
+		foreach (var line in input)
+		{
+			Dictionary<string, int> blockColorMaxCounts = new()
+			{
+				{ "red", 0 },
+				{ "green", 0 },
+				{ "blue", 0 }
+			};
+
+			var modifiedLine = Regex.Replace(line, @"^Game \d+: ", string.Empty);
+
+			int currentBlockCount = 0;
+
+			var lineChunks = modifiedLine
+				.Replace(";", " ;")
+				.Split(' ')
+				.Select(s => 
+					s.Replace(",", string.Empty));
+
+			foreach (var chunk in lineChunks)
+			{
+				// Chunk is a delimiter or blank, continue on with yourself
+				if (chunk == ";" || string.IsNullOrWhiteSpace(chunk))
+				{
+					continue;
+				}
+				// Chunk is a block count
+				if (int.TryParse(chunk, out int blockCount))
+				{
+					currentBlockCount = blockCount;
+				}
+				// Chunk is a block color descriptor
+				else
+				{
+					blockColorMaxCounts[chunk] = 
+						currentBlockCount > blockColorMaxCounts[chunk] ? 
+						currentBlockCount : 
+						blockColorMaxCounts[chunk];
+				}
+			}
+
+			possibleGamePowers +=
+				(blockColorMaxCounts["red"] *
+				blockColorMaxCounts["green"] *
+				blockColorMaxCounts["blue"]);
+		}
+
+		return possibleGamePowers.ToString();
+	}
+}
